f0995cae02e8c4fe56e127b98db67f26061839a4
baliboy20 <williampaulton@yahoo.co.uk>
2018-02-27 15:48:18 +0000

    added download button to visualisations

--- src/app/generators/visitor/visitor.service.ts
+++ src/app/generators/visitor/visitor.service.ts
@@ -1,9 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-// import {pipe} from 'rxjs/observable/p';
 
-import {filter, map, reduce, scan, mergeMap, combineLatest, zip, take, takeUntil, takeWhile, tap} from 'rxjs/operators';
-import {range} from 'rxjs/observable/range';
 import {from} from 'rxjs/observable/from';
 import 'rxjs/add/observable/range';
 import 'rxjs/add/observable/from';
@@ -21,22 +18,10 @@ import 'rxjs/add/operator/count';
 import 'rxjs/add/operator/toArray';
 import 'rxjs/add/operator/reduce';
 import 'rxjs/add/operator/do';
-import {interval} from 'rxjs/observable/interval';
 import * as moment from 'moment';
-import {Duration, Moment} from 'moment';
-import {flatMap} from 'tslint/lib/utils';
-import {zipAll} from 'rxjs/operator/zipAll';
 import {Observer} from 'rxjs/Observer';
-import {merge} from 'rxjs/operator/merge';
-import {mergeAll} from 'rxjs/operator/mergeAll';
-import {concat} from 'rxjs/operator/concat';
-import {groupBy} from 'rxjs/operator/groupBy';
-import {of} from 'rxjs/observable/of';
-import {GroupedObservable} from 'rxjs/operators/groupBy';
-import {toArray} from 'rxjs/operator/toArray';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {ReplaySubject} from 'rxjs/ReplaySubject';
 import {error} from 'util';
+import {reduce} from 'rxjs/operators';
 
 // import {let} from 'rxjs/operators';
 
@@ -59,6 +44,9 @@ export class VisitorService {
 
 }
 
+/**
+ *                          INTERFACES
+ */
 interface Dayts {
     date: string;
     dateBucket: string;
@@ -112,7 +100,39 @@ const computeVisitors = (month, isoWeekday, trades) => {
     return tr;
 };
 
-const format2Decimals = (value) => value.toFixed(2);
+// const format2Decimals = (value) => value.toFixed(2);
+export class FixedCostsImpl implements FixedCosts {
+    configName = 'default';
+    serviceCharge: { amount: 300, frequency: 'q', dayDue: 30 };
+    rent = { amount: 300, frequency: 'q', dayDue: 30 };
+    rates = { amount: 300, frequency: 'q', dayDue: 30 };
+    lease = { amount: 300, frequency: 'q', dayDue: 30 };
+}
+
+export interface FixedCosts {
+    configName: string;
+    serviceCharge: {
+        amount: number,
+        frequency: string, // due on
+        dayDue: number,
+    };
+    rent: {amount: number, frequency: string, dayDue: number};
+    rates: {amount: number, frequency: string, dayDue: number};
+    lease: {amount: number, frequency: string, dayDue: number};
+}
+
+export class StoreLocalSettings {
+    static settingName = 'thackSettings';
+    save(value: any[]) {
+        const str = JSON.stringify(value);
+        window.localStorage.setItem(StoreLocalSettings.settingName, str);
+    }
+    retrieve(key) {
+        const data = window.localStorage.getItem(StoreLocalSettings.settingName);
+        return JSON.parse(data);
+    }
+}
+
 
 const computeFixedCosts: (arg: VisitorSequent) => CostSalesSequent = (arg: VisitorSequent) => {
     const v: number[] = [3, 6, 9, 12];
@@ -140,6 +160,7 @@ const computeTotalCosts: (a: CostSalesSequent) => CostSalesSequent = (a: CostSal
     a.profit = a.netSales - a.totalCosts;
     return a;
 };
+
 const computeCumTotals: (a: CostSalesSequent, b: CostSalesSequent, i: number) => CostSalesSequent =
     (prev: CostSalesSequent, curr: CostSalesSequent, idx: number) => {
 
@@ -159,6 +180,7 @@ const computeCumTotals: (a: CostSalesSequent, b: CostSalesSequent, i: number) =>
         }
         return curr;
     };
+
 const computeNetSales: (a: CostSalesSequent) => CostSalesSequent = (a: CostSalesSequent) => {
     a.netSales = (a as VisitorSequent).trades *
         OperatingCosts.averageSale;
@@ -225,101 +247,6 @@ const OperatingCosts = {
 
 export class ForecastModelBasic {
 
-    static dateMap: Map<string, any[]> = new Map();
-
-    // static call() {
-    //     const source$ = range(0, 10);
-    //     source$.pipe(
-    //         filter(x => x % 2 === 0),
-    //         map(x => x + x),
-    //         scan((acc, x) => acc + x, 0)
-    //     )
-    //         .subscribe(x => console.log(x + 'dd'));
-    //
-    //     const scan1 = (acc, idx) => acc + idx;
-    //     const merge = combineLatest(obs => Observable.range(1, 2)
-    //         .map(a => a + ':' + obs));
-    //
-    //
-    //     Observable.range(1, 2).pipe(merge).subscribe(a => {
-    //         // a.subscribe(console.log);
-    //         // console.log('hello there', a);
-    //     });
-    // }
-
-    static costs(p1?: Date, p2?: Date) {
-        const s$ = interval();
-
-        const TRANS_TO_DATE = map((cnt: number) => {
-            const now = moment().add(cnt, 'days').set({'hour': 0, 'minute': 0, 'second': 0, 'millisecond': 0});
-            return now;
-        });
-
-        const TAKE_WHILE = takeWhile((a: any) => {
-            const lmt = moment().add(10, 'days').set({'hour': 0, 'minute': 0, 'second': 0, 'millisecond': 0}).toISOString();
-            // console.log('mo', a.format())
-            return a.isSameOrBefore(lmt);
-        });
-
-        const PIPE = s$.pipe(TRANS_TO_DATE,
-            TAKE_WHILE,
-            take(20)
-        );
-
-        // sub.subscribe(a => a);
-        // console.log(moment.locale());
-        return PIPE;
-    }
-
-    static costsByPeriod(periodType: string, start: string, finish: string) {
-        const increr = scan((acc, curr) => {
-            acc.total *= 1.01;
-            acc.date = curr.date;
-            return acc;
-        }, {date: '', total: 100});
-
-        const dates$ = this.costs();
-        const d2$ = dates$.pipe(map(a => {
-            return {date: a.toISOString(), total: 0};
-        }), increr);
-        return d2$;
-
-
-    }
-
-    static _dateGen() {
-
-    }
-
-    static _dateGen_() {
-        const increr = scan((acc, curr) => {
-            acc.total *= 1.01;
-            acc.date = curr.date;
-            return acc;
-        }, {date: '', total: 100});
-
-        const mapper = a => {
-            return {date: a.toISOString(), total: 0};
-        };
-
-        const one = from(['a', 'b', 'c']);
-        const func1 = (a) => Observable.from(one).map(b => ({date: a.date, one: b}));
-        const func2 = (a) => Observable.from(two).map(b => b);
-        const two = from(['e', 'f', 'g']);
-        const source = range(1, 3);
-        const flowtrans = map(a => ({date: a, one: [], two: []}));
-        const zip1 = zip(func1);
-        const zip2 = zip(func2);
-        // source.pipe(flowtrans, zip1).subscribe(a => a.subscribe(console.log));
-        const m3 = map(() => ({key: 'a', value: 'someval'}));
-        const z3 = zip(range(4, 1), range(9, 1));
-        // const r3 = range(1, 3).pipe( z3).subscribe(console.log);
-
-        const costs = (a) => Observable.of(this._varCosts(a.date)).map(b => ({date: a.date, value: b, label: 'costs'}));
-        const rev = (a) => Observable.of(this._varRevs(a.date)).map(b => ({date: a.date, value: b, label: 'revenue'}));
-        const zip4 = zip(costs, rev);
-        this.zzMergeAll();
-    }
 
     private static _varCosts(date: string): number {
         const dayOfYr = moment(date).diff('2018-01-01', 'days');
@@ -392,61 +319,6 @@ export class ForecastModelBasic {
         );
     }
 
-    /**
-     *
-     */
-    static zzMergeAll() {
-        const costs = (a) => ({date: a, value: this._varCosts(a.date), label: 'costs'});
-
-        const rev = (a) => ({date: a, value: this._varRevs(a.date), label: 'revenue'});
-        const op = (...stms) => {
-            return (source) => {
-                return Observable.create(subscriber => {
-                    const subscription = source.subscribe(value => {
-                        stms.forEach(func => {
-                            subscriber.next((func(value.date)));
-                            console.log('....=>');
-                        });
-                    });
-                    return subscription;
-                });
-            };
-        };
-
-        const reducer = reduce((x, y) => {
-            const acc = {key: '', sum: 0, values: []};
-            // console.log(y);
-            acc.sum = y['value'];
-            acc.values.push(y);
-            acc.key = 'tobeadded';
-            return acc;
-        }, {sum: 0, key: '', values: []});
-
-        const writer = a => a.subscribe(b => console.log('sub subscribe', b));
-
-
-        const sumr = (a: GroupedObservable<any, any>) => {
-            // console.log('sumr', a);
-            return a.reduce((x, y) => x + y, 0);
-        };
-        const assigner = (a, b) => {
-            a['key'] = b['key'];
-            return a;
-        };
-
-        // this.obsDateGenerator('01/01/2018', '5/05/2018')
-        //     .pipe(op(costs, rev))
-        //     // .do(console.log)
-        //     .groupBy(a => moment(a['date']).dayOfYear())
-        //     // .flatMap(a => a.toArray())
-        //     // .subscribe((console.log));
-        //     .subscribe(obs => obs.pipe(take(2), map(a => {
-        //         return assigner(a, obs);
-        //     })).subscribe(console.log));
-
-    }
-
-    // static  gapDates: ReplaySubject<any> = new ReplaySubject<any>('value of gapdates');
 
     /**
      *
@@ -460,20 +332,6 @@ export class ForecastModelBasic {
 
     static computeVisitors(baseVisitorNo: number, monthno: number, isoWeekday: 1 | 2 | 3 | 4 | 5 | 6 | 7) {
         return (baseVisitorNo * weeklyVisitorBias[isoWeekday] * monthlyVisitorBias[monthno]).toPrecision(2);
-    }
-
-    static callZip() {
-        console.clear();
-        // todo addin map cache
-
-        this.fromDateObservable('01-08-2018', '06-28-2018', 'month')
-            .map((a: DateSequent) => VisitorSequentFactory(a, 100))
-            .map(a => computeFixedCosts(a))
-            .map(a => computeVariableCosts(a))
-            .map(a => computeNetSales(a))
-            .subscribe(console.log);
-
-        console.log('day', moment().get('day'));
     }
 
     static baseModel(from1: string = '02/01/2018', to: string = '26/01/2018',
@@ -522,7 +380,7 @@ const computeGroupOnPeriod = (a: any, b: string) => {
     } else {
         throw error('Group period is not set');
     }
-    console.log('the computer group period is', retval, a);
+   // console.log('the computer group period is', retval, a);
     return retval;
 };
 
--- src/app/visualisations/visualisations.component.ts
+++ src/app/visualisations/visualisations.component.ts
@@ -1,13 +1,15 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {CostSalesSequent, VisitorService} from '../generators/visitor/visitor.service';
 import {Observable} from 'rxjs/Observable';
 import {ReplaySubject} from 'rxjs/ReplaySubject';
 import 'rxjs/add/operator/throttle';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/debounce';
-import 'rxjs/operators/windowTime';
+import 'rxjs/operators/scan';
 import 'rxjs/add/observable/interval';
 import {MatRadioGroup} from '@angular/material';
+import moment = require('moment');
+import {Observer} from 'rxjs/Observer';
 
 @Component({
     selector: 'app-visualisations',
@@ -33,7 +35,7 @@ export class VisualisationsComponent implements OnInit {
     _from = new Date('02/13/2018');
     _to = new Date('08/13/2018');
 
-    constructor(private  service: VisitorService) {
+    constructor(private  service: VisitorService, private rnd: Renderer2, private  ele: ElementRef) {
     }
 
     rgChanged(event) {
@@ -43,6 +45,7 @@ export class VisualisationsComponent implements OnInit {
     }
 
     ngOnInit() {
+
     }
 
     runOrRerun() {
@@ -52,8 +55,47 @@ export class VisualisationsComponent implements OnInit {
             null);
 
         obs.do(() => this.data$ = []).toArray()
-    .subscribe(b => this.data$ = b, (err) => console.log, () => console.log('completed') );
+            .subscribe(b => this.data$ = b, (err) => console.log, () => console.log('completed'));
 
+
+    }
+
+    click_download() {
+
+
+        const toCommaDelim = (a) => (`${a.trades},${a.netSales},${a.profit},${a.vatOnSales},${a.variableCosts},${a.date}}`);
+        const toTabs = (a) => (`${a.trades} \t${a.netSales} \t${a.profit} \t${a.vatOnSales} \t${a.variableCosts} \t${a.date} \t\n`);
+        const addTitles = `Trades \tNet Sales \tProfit \tVat on Sales \tVariable Costs \tDate\n`;
+        const fmt2dec = (a) => {
+            const arr = ['trades', 'netSales', 'profit', 'vatOnSales', 'variableCosts'];
+            // console.log('inside fm2dec', a)
+                arr.forEach(b => {
+                    // console.log('a[b]', b, a);
+                    a[b] = (+a[b]).toFixed(2);
+                });
+        return a;
+        };
+        const addMeta = `Date from: ${moment(this._from).format('DDMMMYY')}\t` +
+            `to: ${moment(this._to).format('DDMMMYY')}\t Grouped by:${this.selectedGroupBy}\n`;
+        let data = 'xxx';
+        const obs = this.service.getForecastGrouped(this._from.toISOString(),
+            this._to.toISOString(),
+            this.selectedGroupBy,
+            null);
+
+        obs.do(() => this.data$ = [])
+            .map(fmt2dec)
+            .map(toTabs)
+            .toArray()
+            .do(a => a.unshift(addTitles))
+            .do(a => a.unshift(addMeta))
+            // .do((a) => console.log(addMeta))
+            .subscribe(b => {
+                data = b.toString().replace(/\,/g, '');
+                data.replace('0', 'z');
+                console.log('data', data);
+
+            }, console.log, () => this.createDownloadElement(data));
 
     }
 
@@ -61,9 +103,27 @@ export class VisualisationsComponent implements OnInit {
         if (to === 'from') {
             this._from = event.target.value;
         } else {
-           this._to = event.target.value;
+            this._to = event.target.value;
         }
         this.runOrRerun();
+    }
+
+    createDownloadElement(value) {
+        console.log('DOWNLOADING...');
+        const rn = this.rnd;
+        const ele1: HTMLAnchorElement = rn.createElement('a') as HTMLAnchorElement;
+        rn.appendChild(this.ele.nativeElement, ele1);
+        // const dataUri = 'data:text/plain;base64,' + btoa('here we go again');
+        const blob: Blob = new Blob([value], {type: 'text/plain'});
+        const url = window.URL.createObjectURL(blob);
+        ele1.href = url;
+        ele1.download = 'xlDownload';
+        ele1.click();
+        // window.URL.revokeObjectURL(blob);
+        // window.open(url);
+
+        // const a: File
+        rn.removeChild(this.ele.nativeElement, ele1);
     }
 
 }
